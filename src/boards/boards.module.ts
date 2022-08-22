4468fb5b0420c7e021533233f0832765afd047fd
gabriel <gabriel utzig>
2022-08-22 17:31:28 -0300

    add user service as a provider

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersSchema } from 'src/users/schemas/users.schema';
+import { UsersModule } from 'src/users/users.module';
+import { UsersService } from 'src/users/users.service';
 import { BoardsService } from './boards.service';
 import { BoardsController } from './controllers/boards.controller';
 import { BoardsSchema } from './schemas/boards.schema';
 
 @Module({
-  providers: [BoardsService],
+  providers: [BoardsService, UsersService],
   controllers: [BoardsController],
   imports: [
+    UsersModule,
     MongooseModule.forFeature([{ name: 'Boards', schema: BoardsSchema }]),
+    MongooseModule.forFeature([{ name: 'Users', schema: UsersSchema }]),
   ],
 })
 export class BoardsModule {}
