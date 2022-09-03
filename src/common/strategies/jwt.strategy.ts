a2423b7b8c3d9a5376731f30a8df83ad79f06abb
gabriel <gabriel utzig>
2022-09-03 00:47:59 -0300

    make JWT passaport parse cookies and baerer tokens

--- src/common/strategies/jwt.strategy.ts
+++ src/common/strategies/jwt.strategy.ts
@@ -1,12 +1,24 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { Request } from "express";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      //jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([(request:Request) => {
+        const data = request?.cookies["jwt"];
+        if(!data){
+          try{
+            return request?.headers.authorization.replace('Bearer ','');
+          }catch { 
+            return null;
+          }
+        }
+        return data;
+      }]),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET,
     });
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,7 @@ async function bootstrap() {
     methods: ['POST', 'GET'],
     credentials: true
   });
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
