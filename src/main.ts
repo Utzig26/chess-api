b0051416ce23d6b937c89da4ab4be7b9baa8a0d5
gabriel <gabriel utzig>
2022-09-02 23:34:26 -0300

    add CORS origin .env var

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors( { 
+    origin: [process.env.CORS_ORIGIN],
+    methods: ['POST', 'GET'],
+    credentials: true
+  });
   await app.listen(3000);
 }
 bootstrap();
