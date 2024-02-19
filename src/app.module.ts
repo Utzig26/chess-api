0efcc120b554cfa92b435c5b839717af064a9627
Utzig26 <gabriel.utzig26@gmail.com>
2024-02-18 22:58:56 -0300

    refactor:app resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AuthController } from './auth/auth.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@localhost:27017/chess-api?authSource=admin`
+      ),
+    AuthModule,
+    UsersModule
+  ],
+  controllers: [AuthController],
+  providers: []
 })
-export class AppModule {}
+export class AppModule { }
