eb44499a85fdfaf5365b85e3a6dacb16ade826d7
Utzig26 <gabriel.utzig26@gmail.com>
2025-04-04 18:12:41 -0300

    todo: cookiepareser to httponlycookie auth

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // todo: add cookie parser
+  // app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
