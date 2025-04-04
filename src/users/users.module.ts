ed93a2f3f377311201003dc9c2355ab8241babe4
Utzig26 <gabriel.utzig26@gmail.com>
2025-04-04 18:11:44 -0300

    feat: mock password check

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
-import { User, UserSchema } from './schema/user.sschema';
+import { User, UserSchema } from './schema/user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
-//import { UsersController } from './users.controller';
 import { UsersController } from './users.controller';
 
 @Module({
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from './schema/user.sschema';
+import { User, UserDocument } from './schema/user.schema';
 import { CreateUserDto } from './dto/users.create.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectModel(User.name)
-    private userModel: Model<User>,
-  ) {}
+  logger: Logger;
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {
+    this.logger = new Logger(UsersService.name);
+  }
 
   async findOne(username: string): Promise<User> {
-    return this.userModel.findOne({ username }).exec();
+    return this.userModel.findOne({ username: username }).exec();
+  }
+
+  async checkPassword(username: string, password: string): Promise<User> {
+    const user = await this.userModel
+      .findOne({ username: username })
+      .select('+password')
+      .exec();
+
+    if (!user) return null;
+    if (user.password !== password) return null;
+
+    return user;
   }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     const createdUser = new this.userModel(createUserDto);
-
     return await createdUser.save();
   }
 }
