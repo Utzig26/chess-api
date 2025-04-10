88b7e4f22de30fa5cc3c49b1680534ce18dd7d04
Utzig26 <gabriel.utzig26@gmail.com>
2025-04-09 23:21:32 -0300

    feat: add MongoDB exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MongoExceptionFilter } from './utils/filters/mongo.globalFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // todo: add cookie parser
   // app.use(cookieParser());
+  app.useGlobalFilters(new MongoExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/utils/filters/mongo.globalFilter.ts
@@ -0,0 +1,19 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ConflictException,
+  ExceptionFilter,
+} from '@nestjs/common';
+import { MongoError } from 'mongodb';
+
+@Catch(MongoError)
+export class MongoExceptionFilter implements ExceptionFilter {
+  catch(exception: MongoError, host: ArgumentsHost) {
+    switch (exception.code) {
+      case 11000:
+        throw new ConflictException({
+          message: `Duplicate key error`,
+        });
+    }
+  }
+}
