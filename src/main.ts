f5cc389ab3084bc33a3fa85e1c78f9eba0b8de6e
Utzig26 <gabriel.utzig26@gmail.com>
2025-07-23 17:27:20 -0300

    feat: adding the filters and interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MongoExceptionFilter } from './utils/filters/mongo.globalFilter';
+import { MongoExceptionFilter } from './utils/filters/mongo.global.exception.filter';
+import { GlobalExceptionFilter } from './utils/filters/global.exception.filter';
+import { GlobalResponseInterceptor } from './utils/interpectors/global.response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // todo: add cookie parser
   // app.use(cookieParser());
-  app.useGlobalFilters(new MongoExceptionFilter());
+  app.useGlobalFilters(new GlobalExceptionFilter(), new MongoExceptionFilter());
+  app.useGlobalInterceptors(new GlobalResponseInterceptor());
   await app.listen(3000);
 }
 bootstrap();
