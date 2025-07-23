462a326036afd31d858b76a6d9ed2db5ff9a4af5
Utzig26 <gabriel.utzig26@gmail.com>
2025-07-23 17:27:01 -0300

    feat: using the warp message decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,22 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
 import { UserCreateDto } from 'src/users/dto/user.create.dto';
 import { UserSignDto } from '../users/dto/user.sign.dto';
+import { WrapMessage } from '../utils/decorators/wrap-message.decorator';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private authService: AuthService,
-    //private usersService: UsersService
-  ) {}
+  constructor(private authService: AuthService) {}
 
   @Post('signup')
+  @WrapMessage('User created')
   async signUp(
     @Body(ValidationPipe) userCreateDto: UserCreateDto,
   ): Promise<{ access_token: string }> {
-    const jwt = await this.authService.signUp(userCreateDto);
-    return jwt;
+    return await this.authService.signUp(userCreateDto);
   }
 
+  @WrapMessage('User signed in')
   @Post('signin')
   async signIn(
     @Body(ValidationPipe) userSignDto: UserSignDto,
